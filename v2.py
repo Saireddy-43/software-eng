87df7497df283132e44f9c9baf453de0421fb811
sai somanath reddy <skanduku4@gitam.in>
2025-01-24 13:40:05 +0530

    v2

---
+++ v2.py
@@ -0,0 +1,7 @@
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+c=int(input("enter the value of c:"))
+x=int(input("enter the value of x:"))
+y=0
+y = a*(x*x)+b*x+c;
+print("for the values:a",a,"b",b,"c",c,"y is:",y)
